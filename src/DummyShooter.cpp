1c034c07342238fd89f7e97f13f300f9ed1f64d5
MisterPup <clapuclapu@tiscali.it>
2013-03-28 15:36:00 +0100

    Added file globalVariables.h

--- src/DummyShooter.cpp
+++ src/DummyShooter.cpp
@@ -13,48 +13,9 @@
 #include "Player2D.h"
 #include "Bullet.h"
 #include "BulletSystem.h"
+#include "globalVariables.h"
 
 using namespace std;
-
-//Window dimensions
-int screenX = 1024;
-int screenY = 768;
-
-//World boundaries
-const float topWorld = 4.0f;
-const float bottomWorld = -topWorld;
-const float westWorld = 5.0f;
-const float eastWorld = -westWorld;
-
-//Player
-Player2D* player1;
-float bodyDimX = 1.0f;
-float bodyDimY = 0.2f;
-
-//Gun
-float gunDimX = 0.2f;
-float gunDimY = 0.3f;
-
-//Position of player (except shiftY)
-float shiftX = 0.0f;
-float shiftY = bottomWorld + bodyDimY/2.0f; //when the bullet is fired, the y axis needs to be set between the gun and the player (for rotation purpose - look at bullet's draw code)
-float shiftZ = 0.0f;
-float gunRotation = 0.0;
-
-//Bullet
-BulletSystem* bulletSystem;
-float bulletDimY = 0.1f;
-float bulletSpeed = 0.1f;
-int numBullets = 5;
-
-//Update time
-int FPS = 30;
-float updateTime = 1000/(float)FPS;
-
-float worldBoundaries[4] = {topWorld, bottomWorld, westWorld, eastWorld};
-
-//Gun Rotation Limits
-const float rotationsLimit = 45.0f;
 
 //Called when a special key is pressed
 void handleSpecialKeyPress(int key, int x, int y) 
